49bfaa5aa56f4800335102c31a0adce9dc674c21
Lanxiang Zhang <zlx930422@gmail.com>
2024-08-23 00:14:03 +0300

    Update the API Gateway.

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -58,3 +58,25 @@ resource "aws_lambda_permission" "lambda_permission" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.webserver-api.execution_arn}/*/*/*"
 }
+
+resource "aws_api_gateway_resource" "file_resource" {
+  rest_api_id = aws_api_gateway_rest_api.webserver-api.id
+  parent_id   = aws_api_gateway_rest_api.webserver-api.root_resource_id
+  path_part   = "file"
+}
+
+resource "aws_api_gateway_method" "file_method" {
+  rest_api_id   = aws_api_gateway_rest_api.webserver-api.id
+  resource_id   = aws_api_gateway_resource.file_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "file_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.webserver-api.id
+  resource_id             = aws_api_gateway_resource.file_resource.id
+  http_method             = aws_api_gateway_method.file_method.http_method
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:s3:::${aws_s3_bucket.file_bucket.bucket}/*"
+}
