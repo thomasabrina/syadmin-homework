0f6992243a59cb002364f5039f3582f7988f5bf8
Lanxiang Zhang <zlx930422@gmail.com>
2024-08-22 23:54:24 +0300

    Create an S3 Bucket.

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -6,3 +6,14 @@
 #  content                = data.template_file.textfile.rendered
 #  server_side_encryption = "AES256"
 #}
+resource "aws_s3_bucket" "file_bucket" {
+  bucket = "lxz_bucket"  
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_object" "file" {
+  bucket = aws_s3_bucket.file_bucket.id
+  key    = "lxz-file.txt"  
+  source = "terraform/files/example.txt" 
+  acl    = "private"
+}
